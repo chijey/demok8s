2e550592a9b8060905510b7ffcf98c4a034d13c6
shiqian <wushiqian@tj167.wecom.work>
2022-11-04 15:18:43 +0800

    test

--- nacos-consumer/src/main/java/com/chijey/nacosconsumer/controller/UserController.java
+++ nacos-consumer/src/main/java/com/chijey/nacosconsumer/controller/UserController.java
@@ -2,6 +2,7 @@ package com.chijey.nacosconsumer.controller;
 
 import com.chijey.helloapi.feign.UserClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,22 @@ class UserController {
     @Autowired
     private UserClient userClient;
 
+	@Value("${useLocalCache:false}")
+	private boolean useLocalCache;
+
     @RequestMapping("/getall")
 	public List<String> getAllGreetings() {
 		List<String> greetings = userClient.getGreetings();
 		return greetings;
+	}
+
+
+
+	/**
+	 * 查看是否获取道nacos配置接口
+	 */
+	@RequestMapping("/get")
+	public boolean get() {
+		return useLocalCache;
 	}
 }
